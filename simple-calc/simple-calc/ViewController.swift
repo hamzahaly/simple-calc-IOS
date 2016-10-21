280e8ba82f88749c6a8b00b81bbce40263dcb374
hamzahaly <hzhaly93@gmail.com>
2016-10-21 16:56:29 -0700

    got some debugging to do

--- simple-calc/simple-calc/ViewController.swift
+++ simple-calc/simple-calc/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     var number: Double = 0.0
     var operation: String = ""
-
+    var results: Double = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,14 +23,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //Numbers that the user taps
     @IBAction func buttonInput(_ sender: UIButton) {
-        number = Double(sender.titleLabel!.text!)!
+        number = number * 10 + Double(sender.titleLabel!.text!)!
         labelResults.text = ("\(Int(number))")
     }
 
+    //Operations that the user taps
     @IBAction func buttonOperation(_ sender: UIButton) {
         print(sender.titleLabel!.text!)
+        operation = sender.titleLabel!.text!
+        operation = "="
+        switch operation {
+        case "=":
+            print("equals")
+            results = number
+            print(results)
+            print(number)
+        case "+":
+            print("adding")
+            results = results + number
+            print(results)
+        case "-":
+            print("subtracting")
+            results = results - number
+        case "*":
+            print("multiplying")
+            results = results * number
+        case "/":
+            print("dividing")
+            results = results / number
+        case "%":
+            print("modding")
+            results = results / number
+        default:
+            print("error")
+        }
+        
+        number = 0
+        labelResults.text = ("\(results)")
+        
     }
     
     @IBOutlet weak var labelResults: UITextField!
+    
 }
